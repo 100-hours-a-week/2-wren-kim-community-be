a245fb4d1fc63a29aa7557b7069b498ebf130d54
Sieun Kim <20210760@dongduk.ac.kr>
2025-03-15 17:27:40 +0900

    Feat: order_index 관련 코드 추가

--- src/main/java/ktb/community/be/domain/post/application/FileStorageService.java
+++ src/main/java/ktb/community/be/domain/post/application/FileStorageService.java
@@ -2,6 +2,7 @@ package ktb.community.be.domain.post.application;
 
 import ktb.community.be.domain.post.domain.Post;
 import ktb.community.be.domain.post.domain.PostImage;
+import ktb.community.be.domain.user.domain.User;
 import ktb.community.be.global.exception.CustomException;
 import ktb.community.be.global.exception.ErrorCode;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.IntStream;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,20 +20,24 @@ public class FileStorageService {
 
     private static final String UPLOAD_DIR = System.getProperty("user.dir") + "/uploads/posts/";
 
-    public List<PostImage> storeImages(List<MultipartFile> images, Post post) {
+    public List<PostImage> storeImages(List<MultipartFile> images, List<Integer> orderIndexes, Post post, User user) {
         if (images == null || images.isEmpty()) return List.of();
 
-        return images.stream()
-                .map(image -> saveFile(image, post))
+        if (orderIndexes == null || orderIndexes.size() != images.size()) {
+            throw new CustomException(ErrorCode.INVALID_REQUEST, "이미지 개수와 orderIndex 개수가 맞지 않습니다.");
+        }
+
+        return IntStream.range(0, images.size())
+                .mapToObj(i -> saveFile(images.get(i), post, user, orderIndexes.get(i)))
                 .collect(Collectors.toList());
     }
 
-    private PostImage saveFile(MultipartFile file, Post post) {
+    private PostImage saveFile(MultipartFile file, Post post, User user, int orderIndex) {
         try {
             ensureUploadDirExists();
             String savedFilePath = generateUniqueFilePath(file);
             file.transferTo(new File(savedFilePath));
-            return new PostImage(post, savedFilePath);
+            return new PostImage(post, user, savedFilePath, orderIndex);
         } catch (IOException e) {
             throw new CustomException(ErrorCode.FILE_UPLOAD_FAILED, "파일 저장 실패: " + file.getOriginalFilename());
         }
