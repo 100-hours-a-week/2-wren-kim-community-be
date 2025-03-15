013c677d55ced7c69714acc330845785b661c21d
Sieun Kim <20210760@dongduk.ac.kr>
2025-03-15 17:57:17 +0900

    Refactor: JOIN FETCH -> Batch size 변경

--- src/main/java/ktb/community/be/domain/comment/dao/PostCommentRepository.java
+++ src/main/java/ktb/community/be/domain/comment/dao/PostCommentRepository.java
@@ -1,6 +1,7 @@
 package ktb.community.be.domain.comment.dao;
 
 import ktb.community.be.domain.comment.domain.PostComment;
+import org.hibernate.annotations.BatchSize;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,10 +9,14 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface PostCommentRepository extends JpaRepository<PostComment, Long> {
-    @Query("SELECT c FROM PostComment c " +
-            "LEFT JOIN FETCH c.user " +
-            "LEFT JOIN FETCH c.parentComment " +
-            "WHERE c.post.id = :postId " +
-            "ORDER BY c.createdAt ASC")
+//    @Query("SELECT c FROM PostComment c " +
+//            "LEFT JOIN FETCH c.user " +
+//            "LEFT JOIN FETCH c.parentComment " +
+//            "WHERE c.post.id = :postId " +
+//            "ORDER BY c.createdAt ASC")
+//    List<PostComment> findAllByPostId(@Param("postId") Long postId);
+
+    @Query("SELECT c FROM PostComment c WHERE c.post.id = :postId AND c.deletedAt IS NULL ORDER BY c.createdAt ASC")
+    @BatchSize(size = 20)
     List<PostComment> findAllByPostId(@Param("postId") Long postId);
 }
