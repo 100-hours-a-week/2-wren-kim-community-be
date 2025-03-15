de33ff2bdc338b13549c05743722b18ed8f1edde
Sieun Kim <20210760@dongduk.ac.kr>
2025-03-15 19:44:25 +0900

    Refactor: 주석 정리

--- src/main/java/ktb/community/be/domain/post/dao/PostRepository.java
+++ src/main/java/ktb/community/be/domain/post/dao/PostRepository.java
@@ -10,7 +10,9 @@ import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    // 게시글 상세 조회
+    /*
+    게시글 상세 조회
+     */
     @EntityGraph(attributePaths = {"user"})
     @Query("SELECT p FROM Post p " +
             "WHERE p.id = :id AND p.deletedAt IS NULL")
