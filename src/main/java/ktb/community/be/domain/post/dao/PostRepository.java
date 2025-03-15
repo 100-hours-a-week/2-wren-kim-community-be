a937189c4358cefa3a5405f34aec9174a69f4ac3
Sieun Kim <20210760@dongduk.ac.kr>
2025-03-15 17:56:43 +0900

    Refactor: author -> user 변경

--- src/main/java/ktb/community/be/domain/post/dao/PostRepository.java
+++ src/main/java/ktb/community/be/domain/post/dao/PostRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     // 게시글 상세 조회
-    @EntityGraph(attributePaths = {"author"})
+    @EntityGraph(attributePaths = {"user"})
     @Query("SELECT p FROM Post p " +
             "WHERE p.id = :id AND p.deletedAt IS NULL")
     Optional<Post> findByIdAndDeletedAtIsNull(@Param("id") Long id);
