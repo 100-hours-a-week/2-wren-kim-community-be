7c2094d31787b206686e1e44116d45048e7e972b
Sieun Kim <20210760@dongduk.ac.kr>
2025-03-19 18:00:11 +0900

    Feat: createdAt 추가

--- src/main/java/ktb/community/be/domain/post/dto/PostDetailResponseDto.java
+++ src/main/java/ktb/community/be/domain/post/dto/PostDetailResponseDto.java
@@ -6,16 +6,18 @@ import ktb.community.be.domain.post.domain.Post;
 import ktb.community.be.domain.post.domain.PostImage;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@JsonPropertyOrder({"id", "title", "content", "viewCount", "likeCount", "commentCount", "memberNickname", "memberProfileImageUrl", "imageUrls", "comments"})
+@JsonPropertyOrder({"id", "title", "content", "createdAt", "viewCount", "likeCount", "commentCount", "memberNickname", "memberProfileImageUrl", "imageUrls", "comments"})
 @Getter
 public class PostDetailResponseDto {
 
     private Long id;
     private String title;
     private String content;
+    private LocalDateTime createdAt;
     private int viewCount;
     private int likeCount;
     private int commentCount;
@@ -28,6 +30,7 @@ public class PostDetailResponseDto {
         this.id = post.getId();
         this.title = post.getTitle();
         this.content = post.getContent();
+        this.createdAt = post.getCreatedAt();
         this.viewCount = post.getViewCount();
         this.likeCount = likeCount; // 직접 조회한 값 사용
         this.commentCount = post.getCommentCount();
