89067b6a079da57851c4cfb6d0273e1a164b13b1
Sieun Kim <20210760@dongduk.ac.kr>
2025-03-19 14:09:00 +0900

    Feat: corsConfigurer 추가

--- src/main/java/ktb/community/be/global/config/CorsConfig.java
+++ src/main/java/ktb/community/be/global/config/CorsConfig.java
@@ -5,9 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
+                        .allowedOrigins("http://localhost:5173") // 프론트엔드 서버 주소
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                        .allowCredentials(true); // 쿠키, 인증 정보 포함 가능
+            }
+        };
+    }
 
     @Bean
     public CorsFilter corsFilter() {
