95a787ee6c49663eb9308ca73de0ae3366da6bae
Sieun Kim <20210760@dongduk.ac.kr>
2025-03-20 10:08:22 +0900

    Fix: 프론트 주소 통일

--- src/main/java/ktb/community/be/global/config/WebConfig.java
+++ src/main/java/ktb/community/be/global/config/WebConfig.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/auth/**")
-                .allowedOrigins("http://127.0.0.1:54207")
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:*", "http://127.0.0.1:*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept")
                 .allowCredentials(true);
