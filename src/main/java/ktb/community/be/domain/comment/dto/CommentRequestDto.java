15514ff801a25aeff1bedceeae72fb0c525ec72a
Sieun Kim <20210760@dongduk.ac.kr>
2025-03-26 09:31:32 +0900

    Feat: content 필드 @NotNull, @NotBlank 추가

--- src/main/java/ktb/community/be/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/ktb/community/be/domain/comment/dto/CommentRequestDto.java
@@ -1,5 +1,7 @@
 package ktb.community.be.domain.comment.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,5 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CommentRequestDto {
     private Long memberId;
+
+    @NotNull(message = "내용을 입력해주세요.")
+    @NotBlank(message = "내용은 공백일 수 없습니다.")
     private String content;
 }
